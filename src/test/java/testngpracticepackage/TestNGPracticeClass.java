f5dab6f7ea5f0dc54ff1133aa548693e5f88a953
admin <admin@10.0.0.243>
2017-08-12 17:38:17 -0500

    Initial Update

---
+++ src/test/java/testngpracticepackage/TestNGPracticeClass.java
@@ -0,0 +1,12 @@
+package testngpracticepackage;
+
+import org.testng.annotations.Test;
+
+public class TestNGPracticeClass {
+  @Test
+  public void f() {
+  
+  System.out.println("Practice");
+	  
+  }
+}
